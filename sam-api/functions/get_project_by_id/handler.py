3c650d01363717117ee8ae539ed89458ca3e3567
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-07-28 23:24:42 +0700

    update lambda

--- sam-api/functions/get_project_by_id/handler.py
+++ sam-api/functions/get_project_by_id/handler.py
@@ -1,10 +1,22 @@
 import json
 import requests
+import boto3
+import os
+
+# connect to dynamoDb
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['TABLE_NAME']
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "get a project path",
-        }),
-    }
+    scan_result = dynamo.scan(TableName=table_name)
+    return respond(None, scan_result)
