775dc41c429c4bc9220f7c010b2f68f973c387d4
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-07-29 17:04:52 +0700

    update create_new_project function

--- sam-api/functions/create_new_project/handler.py
+++ sam-api/functions/create_new_project/handler.py
@@ -1,7 +1,8 @@
 import json
-import requests
 import boto3
 import os
+import datetime
+import re
 
 # connect with SQS
 print('connecting with SQS service')
@@ -17,12 +18,42 @@ def respond(err, res=None):
         }
     }
 
+def get_owner_and_project(github_url):
+    result = re.search(r"^https:\/\/github.com\/([a-z0-9A-Z]+)\/(([\w\d]+))", github_url)
+    group = result.groups()
+    owner = group[0]
+    projec = group[1]
+    return (owner, projec)
+
 def lambda_handler(event, context):
-    print('start sending queue')
+    # Get github url from body
+    github_url = event['github_url']
+    requested_time = datetime.datetime.now()
+    
+    # Attract full_url, project, owner from the link
+    owner_name, project_name = get_owner_and_project(github_url)
+    
+    # Create queue with body and additional attributes
+    print('start sending queue', github_url, owner_name, project_name)
     response = sqs.send_message(
         QueueUrl=queue_name,
-        MessageBody='send q to new project q',
-        DelaySeconds=500
+        MessageBody=github_url,
+        MessageDeduplicationId='github',
+        MessageGroupId='github',
+        MessageAttributes={
+            'owner_name': {
+                'StringValue': owner_name,
+                'DataType': 'String'
+            },
+            'project_name': {
+                'StringValue': project_name,
+                'DataType': 'String'
+            },
+            'requested_time': {
+                'StringValue': str(requested_time),
+                'DataType': 'String'
+            }
+        }
     )
 
     return respond(None, response)
