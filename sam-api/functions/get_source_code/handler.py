59f28470a7c844ac14094190262756bd964f7048
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-05 11:14:05 +0700

    update get_source_code function

---
+++ sam-api/functions/get_source_code/handler.py
@@ -0,0 +1,37 @@
+import os
+import json
+import boto3
+
+# Connect to S3
+bucket_name = os.environ['BUCKET_NAME']
+s3 = boto3.client('s3')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
+def lambda_handler(event, context):
+    # Get repo and owner from event prop
+    repo = event['project']
+    owner = event['owner']
+    
+    # Clone source code to tmp folder
+    github_url = f"https://github.com/{owner}/{repo}.git"
+    tmp_folder = f"."
+    folder_name = f"{repo}/"
+
+    # # Upload code to s3 bucket
+    for root, dirs, files in os.walk(tmp_folder):
+        for file in files:
+            s3.upload_file(os.path.join(root, file), bucket_name, folder_name + file)
+
+    # # TODO: Create queue to inform completion
+
+    return respond(None, {
+        'github_url': 'github_url'
+    })
