f7b4f952013fa543631fd7947ec0acfa8f259c5a
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-13 22:46:10 +0700

    update analyser function

--- sam-api/functions/analyser_controller/get_popularity_rating.py
+++ sam-api/functions/analyser_controller/get_popularity_rating.py
@@ -1,6 +1,16 @@
-def get_value():
-    return 100
 
+# Metric: Popularity Rating Score
+# Description: How much repository popularity
 
-def get_score():
-    return 100
+def get_value(data):
+    if data is None:
+        return (0,0)
+
+    start = data['stargazers_count'] if 'stargazers_count' in data else 0
+    number_watcher = data['watchers'] if 'watchers' in data else 0
+
+    return (start, number_watcher)
+
+def get_score(values):
+    star, number_of_watcher = values
+    return (star / number_of_watcher) * 100
--- sam-api/functions/analyser_controller/get_testibility.py
+++ sam-api/functions/analyser_controller/get_testibility.py
@@ -1,6 +1,28 @@
-def get_value():
-    return 100
+def find_metric(arr, key):
+    for item in arr:
+        if item['metric'] == key:
+            return item['value']
+
+def get_value(data):
+    measures = data['component']['measures']
+    cyclo = find_metric(measures, 'complexity')
+    function = find_metric(measures, 'functions')
+    return int(cyclo)/int(function)
 
 
-def get_score():
-    return 100
+def get_score(avg_cyclimetric):
+    if avg_cyclimetric is None:
+        avg_cyclimetric = 1
+
+    rank = 1
+
+    if avg_cyclimetric > 50:
+        rank = 1
+    elif avg_cyclimetric >= 21 and avg_cyclimetric <= 50:
+        rank = 2
+    elif avg_cyclimetric >= 11 and avg_cyclimetric <= 20:
+        rank = 3
+    elif avg_cyclimetric >= 1 and avg_cyclimetric <= 10:
+        rank = 4  
+
+    return (rank / 4) * 100
