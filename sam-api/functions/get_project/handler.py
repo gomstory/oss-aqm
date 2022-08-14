d5957f32f700039be269a03f7dae3309a41ed9c9
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-15 02:28:50 +0700

    revise function name to be clear

---
+++ sam-api/functions/get_project/handler.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import boto3
+import os
+
+# connect to dynamoDb
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['CRAWLER_TABLE']
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
+def lambda_handler(event, context):
+    scan_result = dynamo.scan(TableName=table_name)
+    return respond(None, scan_result)
