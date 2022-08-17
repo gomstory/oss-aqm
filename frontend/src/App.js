a3b17926defd7945e2466483515c633a95a8bcbe
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-18 01:40:38 +0700

    update react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './common/header';
 import Navbar from './common/navbar';
-import CompareProject from './compare-project/compare-project';
+import { Button } from '@aws-amplify/ui-react';
 
-// TODO:
-// 1. setup route to seperate pages
-// 2. continue working on compare project
-//   2.1 allows to horisontal scroll when projects have too many
-//   2.2 setup all metrics and weight column
-//   2.3 add/remove project atomatically
 function App() {
   return (
     <div className="App">
       <header className="App-container">
+        <Button variation="primary">Hello world</Button>
         <Header></Header>
         <Navbar></Navbar>
         <Outlet/>
