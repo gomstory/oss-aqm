8c6e3e3f71d66116426e1ec75f814058e00b6f87
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-20 02:34:21 +0700

    remove crawler folder due to no longer use

--- crawlers/get_source_code.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from git.repo.base import Repo
-
-repo = "angular"
-owner = "angular"
-
-# Clone source code from github project
-Repo.clone_from(f"https://github.com/{owner}/{repo}", f"./raw-data/{repo}/code")
