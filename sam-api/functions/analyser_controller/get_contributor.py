f30fcc73cfa48a65beb3588cb7f93e819ab2e3e8
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-23 08:51:24 +0700

    add core team to contributor

--- sam-api/functions/analyser_controller/get_contributor.py
+++ sam-api/functions/analyser_controller/get_contributor.py
@@ -5,28 +5,29 @@ Metric: Get Contributor
 Description: The number of core developer and contributor in the last 6 month
 """
 class Contributor(OSS_Calculator):
+    """Aka of Support Contributor"""
     def __init__(self, data: dict) -> None:
         self.contributors = data['contributor'] if 'contributor' in data else []
+        self.core_team = data['core-team'] if 'core-team' in data else []
 
     def get_value(self):
         """Number of Support Contributor + Core Team"""
-        # TODO: Add core team to calculation
-        self.value = len(self.contributors)
+        self.value = len(self.contributors) + len(self.core_team)
         return self.value
 
     def get_score(self):
         score = 0
-        contributors = self.value
+        total_contributors = self.value
             
-        if contributors < 5:
+        if total_contributors < 5:
             score = 1
-        elif contributors >= 5 and contributors <= 10:
+        elif total_contributors >= 5 and total_contributors <= 10:
             score = 2
-        elif contributors > 10 and contributors <= 20:
+        elif total_contributors > 10 and total_contributors <= 20:
             score = 3
-        elif contributors > 20 and contributors <= 50:
+        elif total_contributors > 20 and total_contributors <= 50:
             score = 4
-        elif contributors > 50:
+        elif total_contributors > 50:
             score = 5
 
         self.score =  (score / 5) * 100
