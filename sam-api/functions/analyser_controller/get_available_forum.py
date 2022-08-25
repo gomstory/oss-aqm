053a647e6461c3d72da43e601e66fbbb2f80990b
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-25 20:17:17 +0700

    add available forum to calculation but return 0 now

--- sam-api/functions/analyser_controller/get_available_forum.py
+++ sam-api/functions/analyser_controller/get_available_forum.py
@@ -1,6 +1,11 @@
-def get_value():
-    return 0
+from Calculator import ScoreCalculator
 
+class AvailableForum(ScoreCalculator):
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
 
-def get_score():
-    return 0
+    def get_value(self):
+        return 0
+
+    def get_score(self):
+        return 0
