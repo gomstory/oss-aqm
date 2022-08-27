1998160ba15c0bc487c49c3c8792b090fce063bd
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-08-28 01:25:41 +0700

    fix get user in case that no user found

--- sam-api/functions/get_user/handler.py
+++ sam-api/functions/get_user/handler.py
@@ -32,6 +32,10 @@ def lambda_handler(event, context):
     r = requests.get(github_url)
     soup = BeautifulSoup(r.content, "html.parser")
 
+    # Initial values
+    data['used_by'] = 0
+    data["result"] = []
+
     # Find total repositories that have been using
     used_by_div = soup.find('a', { 
         "class": "btn-link", 
@@ -40,48 +44,49 @@ def lambda_handler(event, context):
         
     if used_by_div is not None:
         used_by_text = used_by_div.text
-        used_by_total = used_by_text.replace("\n", "").replace("Repositories", "").replace(" ", "")
-        data['used_by'] = used_by_total
+        used_by_total = used_by_text.replace("\n", "").replace("Repositories", "").replace(" ", "").replace(",", "")
+        data['used_by'] = int(used_by_total)
         data["result"] = []
 
-    for i in range(max_pages):
-        r = requests.get(github_url)
-        soup = BeautifulSoup(r.content, "html.parser")
+    if data['used_by'] > 0:        
+        for i in range(max_pages):
+            r = requests.get(github_url)
+            soup = BeautifulSoup(r.content, "html.parser")
 
-        # Get repo name from html
-        page_data = [
-            "{}/{}".format(
-                t.find('a', {"data-repository-hovercards-enabled":""}).text,
-                t.find('a', {"data-hovercard-type":"repository"}).text
-            )
+            # Get repo name from html
+            page_data = [
+                "{}/{}".format(
+                    t.find('a', {"data-repository-hovercards-enabled":""}).text,
+                    t.find('a', {"data-hovercard-type":"repository"}).text
+                )
 
-            for t in soup.findAll("div", {"class": "Box-row"})
-        ]
+                for t in soup.findAll("div", {"class": "Box-row"})
+            ]
 
-        # Store user and repo of users
-        for row in page_data:
-            result = re.search(r"([\w\d\-\_]+)\/([\w\d\-\_]+)", row)
+            # Store user and repo of users
+            for row in page_data:
+                result = re.search(r"([\w\d\-\_]+)\/([\w\d\-\_]+)", row)
 
-            if result is not None:                    
-                group = result.groups()
-                owner_key = group[0]
-                repo_key = group[1]
-                data["result"].append({
-                    "github_id": row,
-                    "owner": owner_key,
-                    "repo": repo_key
-                })
+                if result is not None:                    
+                    group = result.groups()
+                    owner_key = group[0]
+                    repo_key = group[1]
+                    data["result"].append({
+                        "github_id": row,
+                        "owner": owner_key,
+                        "repo": repo_key
+                    })
 
-        # Find Next pagination
-        paging_div = soup.find("div", {"class":"paginate-container"}).find('a')
+            # Find Next pagination
+            paging_div = soup.find("div", {"class":"paginate-container"}).find('a')
 
-        if paging_div:
-            github_url = paging_div["href"]
-        else:
-            break
+            if paging_div:
+                github_url = paging_div["href"]
+            else:
+                break
 
-        # Slow down for a sec
-        time.sleep(3)
+            # Slow down for a sec
+            time.sleep(3)
 
     # Create json file to tmp folder
     file_name = 'user.json'
