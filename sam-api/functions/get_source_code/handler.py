0ad0e98501f8ef588758c94bb85b8026e9037180
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2022-09-04 15:06:02 +0700

    check project that pass 7 day already

--- sam-api/functions/get_source_code/handler.py
+++ sam-api/functions/get_source_code/handler.py
@@ -2,6 +2,9 @@ import os
 import json
 import boto3
 import time
+import requests
+from datetime import datetime
+from dateutil import parser, relativedelta
 
 # Connect to Queue
 sqs = boto3.client('sqs')
@@ -24,70 +27,110 @@ def respond(err, res=None):
         }
     }
 
+def get_days(created_date: str):
+    now = datetime.now()
+    temp = parser.parse(created_date)
+    then = datetime(temp.year, temp.month, temp.day)
+    diff = now - then
+    days = diff.days
+    return days
+
+def shoud_analysis_project(owner: str, repo: str):
+    # Get all metrics from SonarQube server
+    sonarqube_name = os.environ["EC2_SONAR_SERVER"]
+    instances = ec2.Instance(sonarqube_name)
+
+    if instances.state["Name"] != "running":
+        raise ValueError("Please turn on the sonarqube server")
+
+    ec2_ip_address = instances.public_ip_address
+    response = requests.get(f'http://{ec2_ip_address}:9000/api/projects/search', 
+        auth=(sonar_username, sonar_password), 
+        params={
+            'projects': f"{owner}:{repo}",
+    })
+
+    data = response.json()
+    project_list = data["components"]
+
+    # No project found, shoud analysis the new project
+    if len(project_list) == 0:
+        return True
+
+    # Project found, did it pass 7 days already
+    last_analysis_days =  get_days(project_list[0]["lastAnalysisDate"])
+    if last_analysis_days >= 7:
+        return True
+    else:
+        return False
+
 # The lambda function requires git layer, checkout template.yml
 def lambda_handler(event, context):
-    # Get repo and owner from event prop
     repo = event['repo']
     owner = event['owner']
-    
-    # Send Command to ec2 instance
-    github_url = f"https://www.github.com/{owner}/{repo}"
-    ssm_response = ssm.send_command( 
-        InstanceIds=[ sonarqube_name ], 
-        DocumentName='AWS-RunShellScript', 
-        Comment=f'{github_url}: clone source code and scan by sonarqube', 
-        Parameters={
-            "commands":[
-                "sodu su -",
-                "cd /home/download",
-                f"git clone {github_url}.git",
-                f"cd {repo}",
-                "/opt/sonar-scanner/bin/sonar-scanner \\",
-                "-Dsonar.host.url=http://localhost:9000 \\",
-                "-Dsonar.scm.provider=git \\",
-                "-Dsonar.sources=. \\",
-                f"-Dsonar.projectKey={owner}:{repo} \\",
-                f"-Dsonar.login={sonar_username} \\",
-                f"-Dsonar.password={sonar_password}",
-                "cd ..",
-                f"rm -rf {repo}"
-            ]
-        }
-    )
-    
-    
-    time.sleep(5)
-    
-    # Get run command status
-    ssm_command_id = ssm_response['Command']['CommandId']
-    ssm_output = ssm.get_command_invocation(
-      CommandId=ssm_command_id,
-      InstanceId=sonarqube_name,
-    )
-    
-    # Waiting command to be finished
-    while (ssm_output["Status"] == "InProgress"):
-        ssm_output = ssm.get_command_invocation(
-          CommandId=ssm_command_id,
-          InstanceId=sonarqube_name,
+    lambda_status = "pending"
+    shoud_analysis = shoud_analysis_project(owner=owner, repo=repo)
+
+    if shoud_analysis is True:
+        # Send Command to ec2 instance
+        github_url = f"https://www.github.com/{owner}/{repo}"
+        ssm_response = ssm.send_command( 
+            InstanceIds=[ sonarqube_name ], 
+            DocumentName='AWS-RunShellScript', 
+            Comment=f'{github_url}: clone source code and scan by sonarqube', 
+            Parameters={
+                "commands":[
+                    "sodu su -",
+                    "cd /home/download",
+                    f"git clone {github_url}.git",
+                    f"cd {repo}",
+                    "/opt/sonar-scanner/bin/sonar-scanner \\",
+                    "-Dsonar.host.url=http://localhost:9000 \\",
+                    "-Dsonar.scm.provider=git \\",
+                    "-Dsonar.sources=. \\",
+                    f"-Dsonar.projectKey={owner}:{repo} \\",
+                    f"-Dsonar.login={sonar_username} \\",
+                    f"-Dsonar.password={sonar_password}",
+                    "cd ..",
+                    f"rm -rf {repo}"
+                ]
+            }
         )
         
-        print(ssm_output["Status"])
         time.sleep(5)
+    
+        # Get run command status
+        ssm_command_id = ssm_response['Command']['CommandId']
+        ssm_output = ssm.get_command_invocation(
+        CommandId=ssm_command_id,
+        InstanceId=sonarqube_name,
+        )
+        
+        # Waiting command to be finished
+        while (ssm_output["Status"] == "InProgress"):
+            ssm_output = ssm.get_command_invocation(
+            CommandId=ssm_command_id,
+            InstanceId=sonarqube_name,
+            )
+            
+            print(ssm_output["Status"])
+            time.sleep(5)
 
-    # Map last status and sent to queue
-    print('SSM Status:', ssm_output["Status"])
-    status_switcher = {
-        "Pending": "pending",
-        "Delayed": "delayed",
-        "Success": "completed",
-        "Cancelled": "cancelled",
-        "TimedOut": "timed-out",
-        "Failed": "failed"
-    }
+        # Map last status and sent to queue
+        print('SSM Status:', ssm_output["Status"])
+        status_switcher = {
+            "Pending": "pending",
+            "Delayed": "delayed",
+            "Success": "completed",
+            "Cancelled": "cancelled",
+            "TimedOut": "timed-out",
+            "Failed": "failed"
+        }
 
-    lambda_status = status_switcher.get(
-        ssm_output["Status"], 'failed')
+        lambda_status = status_switcher.get(
+            ssm_output["Status"], 'failed')
+    else:
+        lambda_status = "completed"
 
     # Add queue to inform completion
     sqs.send_message(
